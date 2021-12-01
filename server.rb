3b309918e35b9befda7077177cd8d626cfee564b
Gail Eddy <gaileddy68@gmail.com>
2021-12-01 13:04:35 -0600

    adds stuff

--- server.rb
+++ server.rb
@@ -49,6 +49,21 @@ namespace '/api/v1' do
   before do
     content_type 'application/json'
   end
+
+  helpers do
+    def base_url
+      @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
+    end
+
+    def json_params
+      begin
+        JSON.parse(request.body.read)
+      rescue
+        halt 400, { message:'Invalid JSON' }.to_json
+      end
+    end
+  end
+
     
   get '/books' do
     books = Book.all
@@ -56,8 +71,29 @@ namespace '/api/v1' do
     [:title, :isbn, :author].each do |filter|
       books = books.send(filter, params[filter]) if params[filter]
     end
+
+    get '/books/:id ' do |id|
+      book = Book.where(id: id).first
+      halt(404, { message:'Book Not Found'}.to_json) unless book
+      BookSerializer.new(book).to_json
+    end
+
+    helpers do
+        def base_url
+          @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
+        end
+    
+        def json_params
+          begin
+            JSON.parse(request.body.read)
+          rescue
+            halt 400, { message:'Invalid JSON' }.to_json
+          end
+        end
+      end
+    
   
-    books.to_json
+    books.map { |book| BookSerializer.new(book) }.to_json
   end
 
 end
